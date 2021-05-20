c1b734224decea2d922537485130fda27feaba6c
LucHoang <hoangsyluc@gmail.com>
2021-05-21 00:14:21 +0700

    add chuc nang forgot password

--- src/main/java/com/casestudy/controller/EmailController.java
+++ src/main/java/com/casestudy/controller/EmailController.java
@@ -1,5 +1,7 @@
 package com.casestudy.controller;
 
+import com.casestudy.model.User;
+import com.casestudy.service.user.AppUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -8,12 +10,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
 
 @Controller
 public class EmailController {
 
     @Autowired
     public JavaMailSender emailSender;
+
+    @Autowired
+    private AppUserService userService;
 
     @GetMapping("/forgotPassword")
     public String form() {
@@ -22,19 +30,29 @@ public class EmailController {
 
     @ResponseBody
     @RequestMapping("/sendEmail")
-    public String sendSimpleEmail(@RequestParam("email") String email) {
-        // Create a Simple MailMessage.
-        SimpleMailMessage message = new SimpleMailMessage();
-
+    public ModelAndView sendSimpleEmail(@RequestParam("email") Optional<String> email, @RequestParam("username") Optional<String> username) {
+        if (username.isPresent() && email.isPresent()) {
+            Optional<User> user = userService.findByUsername(username.get());
+            if (user.get().getEmail().equalsIgnoreCase(email.get())) {
+                // Create a Simple MailMessage.
+                SimpleMailMessage message = new SimpleMailMessage();
 //        message.setTo(MyConstants.FRIEND_EMAIL);
-        message.setTo(email);
-        message.setSubject("Test Simple Email");
-        message.setText("Hello, Im testing Simple Email");
+                message.setTo(email.get());
+                message.setSubject("RETRIEVE YOUR PASSWORD");
+                message.setText("The password for the accoount '" + user.get().getUsername() + "': " + user.get().getPassword());
 
-        // Send Message!
-        this.emailSender.send(message);
+                // Send Message!
+                this.emailSender.send(message);
 
-        return "Email Sent!";
+                ModelAndView modelAndView = new ModelAndView("/views/forgot-password");
+                modelAndView.addObject("success", "Please check your email to receive your password.");
+                return modelAndView;
+            }
+        }
+
+        ModelAndView modelAndView = new ModelAndView("/views/forgot-password");
+        modelAndView.addObject("error", "Incorrect username or email !!!");
+        return modelAndView;
     }
 
 }
